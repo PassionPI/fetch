31de300b547b16be626b40151b5b284bf40bcd75
PassionPI <50688829+PassionPI@users.noreply.github.com>
2024-07-15 12:24:41 +0800

    Update jwt.ts

--- src/middleware/jwt.ts
+++ src/middleware/jwt.ts
@@ -3,10 +3,7 @@ import { Middleware } from "../types";
 export const jwt =
   ({ token }: { token: () => string }): Middleware =>
   async (context, next) => {
-    const headers = new Headers(context.headers);
-    headers.set("Authorization", `Bearer ${token()}`);
-
-    context.headers = headers;
+    context.headers.set("Authorization", `Bearer ${token()}`);
 
     return next();
   };
