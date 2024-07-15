87071b30af4b32797e34bf69734940d4985d701e
PassionPI <50688829+PassionPI@users.noreply.github.com>
2024-07-15 12:00:18 +0800

    Update content.ts

--- src/util/content.ts
+++ src/util/content.ts
@@ -22,13 +22,13 @@ export const parseContent = (response: Response) => {
   const { headers } = response;
 
   if (typeJSON(headers)) {
-    return response.clone.json();
+    return response.clone().json();
   }
 
   if (typeText(headers)) {
     // response.clone().text() 这里只有当后端返回无法解析为文本的时候, 才会报错
     // 例如二进制文件, 无效的 UTF-8 字符序列
-    return response.clone.text();
+    return response.clone().text();
   }
 
   return Promise.resolve();
